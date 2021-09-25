c010fa7b22926d3cacc3f1b6d63876c2bf27093e
unknown <coa2769@gmail.com>
2021-09-25 17:10:33 +0900

    페이지에서 한번만 이미지 소스를 얻어오는 예제까지

--- nodejs-crawler/index.js
+++ nodejs-crawler/index.js
@@ -291,113 +291,113 @@
 //==================================================================================//
 //3-1 이미지 다운로드 준비하기
 
-const xlsx = require('xlsx');
-const puppeteer = require('puppeteer');
-const axios = require('axios');
-const fs = require('fs');
-const add_to_sheet = require('./add_to_sheet');
+// const xlsx = require('xlsx');
+// const puppeteer = require('puppeteer');
+// const axios = require('axios');
+// const fs = require('fs');
+// const add_to_sheet = require('./add_to_sheet');
 
-const workbook = xlsx.readFile('xlsx/data.xlsx');
-const ws = workbook.Sheets.영화목록;
-const records = xlsx.utils.sheet_to_json(ws);
+// const workbook = xlsx.readFile('xlsx/data.xlsx');
+// const ws = workbook.Sheets.영화목록;
+// const records = xlsx.utils.sheet_to_json(ws);
 
-//sync 메서드는 동기식 함수이므로 프로그램의 처음과 끝에만 사용해라.
-fs.readdir('screenshot', (err)=>{
-    if(err){
-        console.error('screenshot폴더가 없어 screenshot폴더를 생성합니다.');
-        fs.mkdirSync('screenshot');
-    }
-});
+// //sync 메서드는 동기식 함수이므로 프로그램의 처음과 끝에만 사용해라.
+// fs.readdir('screenshot', (err)=>{
+//     if(err){
+//         console.error('screenshot폴더가 없어 screenshot폴더를 생성합니다.');
+//         fs.mkdirSync('screenshot');
+//     }
+// });
 
-fs.readdir('poster', (err)=>{
-    if(err){
-        console.error('poster 폴더가 없어 poster 폴더를 생성합니다.');
-        fs.mkdirSync('poster');
-    }
-});
+// fs.readdir('poster', (err)=>{
+//     if(err){
+//         console.error('poster 폴더가 없어 poster 폴더를 생성합니다.');
+//         fs.mkdirSync('poster');
+//     }
+// });
 
-const crawler = async ()=>{
-    try{
-        const browser = await puppeteer.launch({
-            headless : process.env.NODE_ENV === 'production',
-            //브라우저의 크기를 조절한다.
-            args : ['--window-size=1920,1080']
-        }); 
-        //한페이지에서 여러 url을 이동하는 것으로 사람 흉내를 낸다.
-        const page = await browser.newPage();
-        //페이지의 크기를 조절한다.
-        await page.setViewport({
-            width : 1920,
-            height : 1080
-        });
-        //navigator.userAgent에 담을 정보를 입력. 브라우저의 정보가 담긴다.
-        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36");
-        add_to_sheet(ws, 'C1', 's', '평점');
+// const crawler = async ()=>{
+//     try{
+//         const browser = await puppeteer.launch({
+//             headless : process.env.NODE_ENV === 'production',
+//             //브라우저의 크기를 조절한다.
+//             args : ['--window-size=1920,1080']
+//         }); 
+//         //한페이지에서 여러 url을 이동하는 것으로 사람 흉내를 낸다.
+//         const page = await browser.newPage();
+//         //페이지의 크기를 조절한다.
+//         await page.setViewport({
+//             width : 1920,
+//             height : 1080
+//         });
+//         //navigator.userAgent에 담을 정보를 입력. 브라우저의 정보가 담긴다.
+//         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36");
+//         add_to_sheet(ws, 'C1', 's', '평점');
 
-        for(const [i, r] of records.entries()){
-            await page.goto(r.링크);
-            const result = await page.evaluate(()=>{
-                const scoreEl = document.querySelector('.score.score_left .star_score');
-                let score = '';
-                if(scoreEl){
-                    score = scoreEl.textContent;
-                }
+//         for(const [i, r] of records.entries()){
+//             await page.goto(r.링크);
+//             const result = await page.evaluate(()=>{
+//                 const scoreEl = document.querySelector('.score.score_left .star_score');
+//                 let score = '';
+//                 if(scoreEl){
+//                     score = scoreEl.textContent;
+//                 }
 
-                const imgEl = document.querySelector('.poster img');
-                let img = '';
-                if(imgEl){
-                    img = imgEl.src;
-                }
+//                 const imgEl = document.querySelector('.poster img');
+//                 let img = '';
+//                 if(imgEl){
+//                     img = imgEl.src;
+//                 }
 
-                return { score, img };
-            });
+//                 return { score, img };
+//             });
 
-            if(result.score){
-                console.log(r.제목, '평점', result.score.trim());
-                const newCell = 'C' + (i + 2);
-                add_to_sheet(ws, newCell, 'n', result.score.trim());
-            }
+//             if(result.score){
+//                 console.log(r.제목, '평점', result.score.trim());
+//                 const newCell = 'C' + (i + 2);
+//                 add_to_sheet(ws, newCell, 'n', result.score.trim());
+//             }
 
-            if(result.img){
-                //- 페이지를 스크린샷을 찍어 저장하는 코드
-                //fullPage : true <= 화면에 보이지 않는 페이지 전체를 스크린샷을 찍는다.
-                //clip <= 특정 부분을 잘라서 이미지를 저장한다.
-                await page.screenshot({
-                    path : `screenshot/${r.제목}.jpg`, 
-                    // fullPage : true,
-                    clip : { 
-                        x:100,
-                        y:100,
-                        width : 300,
-                        height : 300
-                    }
-                });
+//             if(result.img){
+//                 //- 페이지를 스크린샷을 찍어 저장하는 코드
+//                 //fullPage : true <= 화면에 보이지 않는 페이지 전체를 스크린샷을 찍는다.
+//                 //clip <= 특정 부분을 잘라서 이미지를 저장한다.
+//                 await page.screenshot({
+//                     path : `screenshot/${r.제목}.jpg`, 
+//                     // fullPage : true,
+//                     clip : { 
+//                         x:100,
+//                         y:100,
+//                         width : 300,
+//                         height : 300
+//                     }
+//                 });
                 
 
-                //- 페이지의 포스터를 저장하는 가져와서 저장하는 코드
-                //buffer가 연속적으로 들어있는 자료구조가 arraybuffer이다.
-                //replace(/\?.*$/, '') : 쿼리 스트링을 삭제하는 정규표현식(?부터 문자 끝까지 삭제)
-                //해당 페이지에서는 쿼리 스트링을 삭제하니 큰 이미지를 서버에서 보내줬다. 이는 경험적인 것이다.
-                const imgResult = await axios.get(result.img.replace(/\?.*$/, ''),{ 
-                    responseType : 'arraybuffer'
-                });
-                fs.writeFileSync(`poster/${r.제목}.jpg`, imgResult.data)
-            }
+//                 //- 페이지의 포스터를 저장하는 가져와서 저장하는 코드
+//                 //buffer가 연속적으로 들어있는 자료구조가 arraybuffer이다.
+//                 //replace(/\?.*$/, '') : 쿼리 스트링을 삭제하는 정규표현식(?부터 문자 끝까지 삭제)
+//                 //해당 페이지에서는 쿼리 스트링을 삭제하니 큰 이미지를 서버에서 보내줬다. 이는 경험적인 것이다.
+//                 const imgResult = await axios.get(result.img.replace(/\?.*$/, ''),{ 
+//                     responseType : 'arraybuffer'
+//                 });
+//                 fs.writeFileSync(`poster/${r.제목}.jpg`, imgResult.data)
+//             }
 
-            await page.waitFor(1000);
-        }
+//             await page.waitFor(1000);
+//         }
 
-        await page.close();    
-        await browser.close();
-        xlsx.writeFile(workbook, 'xlsx/result.xlsx');
+//         await page.close();    
+//         await browser.close();
+//         xlsx.writeFile(workbook, 'xlsx/result.xlsx');
         
-    } catch (e){
-        console.error(e);
-    }
+//     } catch (e){
+//         console.error(e);
+//     }
 
-};
+// };
 
-crawler();
+// crawler();
 
 /*
 querySelector과 CSS선택자
@@ -429,3 +429,73 @@ ex) tag.child
 ex) img[width=26]
 
 */
+
+//==================================================================================//
+//4-1 ~ 4-3
+/*
+# 인피니트 스크롤링
+무한히 스크롤을 내릴 수 있으며 스크롤 끝에 도달했을 때 서버에서 데이터를 계속 로드해 오는 페이지
+
+    [ 크롤링 할 때 어려운점 ]
+    스크롤을 내려야만 다음 컨텐츠를 가져오기 때문에 어렵다.
+
+
+# Postman
+서버로 요청을 보내어 답정을 테스트할 때 쓰이는 테스트 도구.
+이번 강의에서는 크롤링할 웹 페이지가 간단한지 판변할 때 쓰인다.
+Postman의 Preview로 하면을 확인하면 크롤러가 보게되는 화면을 볼 수 있다.
+
+
+[ 이번 간의에서 해결할 문제 ]
+    unsplash.com
+    => 브라우저로 요청했을 때는 문제없던 페이지가 Postman으로 요청하니 이미지와 컨텐츠를 전혀 가져오지 못한다.
+    => SPA에서 자주 볼수 있는 문제이다.
+    => Webpack으로 class 명들이 암호화 되어 있다. 이런 경우 class명이 자주 바뀌기도 하므로 체크해줘야 한다.
+
+*/
+
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const fs = require('fs');
+
+const crawler = async ()=>{
+    try{
+        const browser = await puppeteer.launch({headless : false});
+        const page = await browser.newPage();
+        await page.goto('https://unsplash.com');
+        const result = await page.evaluate(()=>{
+            let imgs = [];
+            const imgEls = document.querySelectorAll('._6IG7');
+            if(imgEls.length){
+                imgEls.forEach((v)=>{
+                    //1. 이미지 소스를 얻음
+                    let src = v.querySelector('img.oCCRx').src;
+                    if(src){
+                        imgs.push(src);
+                    }
+                    //2. 해당 tag를 삭제
+                    v.parentElement.removeChild(v);
+                });
+            }
+
+            //3. 스크롤을 내리는 동작을 한다.
+            //기존 로드한 tag는 삭제되었으므로 조금만 내려도 다음 컨텐츠를 가져오게 된다.
+            window.scrollBy(0, 300);
+            return imgs;
+        });
+
+        console.log(result);
+
+        //waitForSelector : 선택자에 해당하는 태그가 로딩될 때 까지 기다림.
+        //만약 30간 기다린 후 선택자를 못 찾으면 timeout 에러가 된다.
+        await page.waitForSelector('._6IG7');
+        console.log('태그 로딩 완료!');
+
+        await page.close();    
+        await browser.close();
+    }catch(e){
+        console.error(e);
+    }
+}
+
+crawler();
