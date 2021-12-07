a4509e27d2f51d8a9409c94d8733455c4d2a2f7a
unknown <coa2769@gmail.com>
2021-12-07 13:55:32 +0900

    노드 크롤러의 6-1~6-3 강의 내용 정리

--- nodejs-crawler/index.js
+++ nodejs-crawler/index.js
@@ -820,43 +820,147 @@ Postman의 Preview로 하면을 확인하면 크롤러가 보게되는 화면을
 //page.keyboard.up : 매개변수로 입력된 키가 down함수로 누르고 있는 상태라면 해제한다.
 
 
+// const puppeteer = require('puppeteer');
+// const dotenv = require('dotenv');
+// dotenv.config(); //.env파일에 작성한 환경변수를 불러와 process.env 에 구성한다.
+
+// const crawler = async ()=>{
+//     try{
+//         const browser = await puppeteer.launch({headless : false, args:['--window-size=1920,1080', '--disable-notifications']});
+//         const page = await browser.newPage();
+//         await page.setViewport({
+//             width : 1080,
+//             height : 1080
+//         });
+//         await page.goto('https://facebook.com');
+
+//         await page.focus('#email');
+//         //대문자 입력 방법
+//         await page.click('#email');
+//         await page.keyboard.down('ShiftLeft');
+//         await page.keyboard.press('KeyZ');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyE');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyR');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyO');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyC');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyH');
+//         await page.waitFor(1000);
+//         await page.keyboard.press('KeyO');
+//         await page.waitFor(1000);
+//         await page.keyboard.up('ShiftLeft');
+        
+//     }catch(e){
+//         console.error(e);
+//     }
+// };
+
+// crawler();
+
+//==================================================================================//
+//6-1, 6-2 프록시 설명과 태그 분석 / 프록시 ip 적용하기
+//크롤러를 테스트 하다보면 해당 사이트에서 차단을 당할 수 있다. 
+//이런 차단의 경우 ip기반으로 이루어진다.
+//해당 강의 내용은 프록시를 이용하여 차단을 우회하는 방법이다.
+
+
+//&& tag selector 정리하기
+
+// const puppeteer = require('puppeteer');
+// const dotenv = require('dotenv');
+// dotenv.config(); //.env파일에 작성한 환경변수를 불러와 process.env 에 구성한다.
+
+// const crawler = async ()=>{
+//     try{
+//         //1. 현재 실행되는 프록시 중 가장 빠른 것을 사용하기 위해 아래 URL에서 프록시 목록을 크롤러로 가져와 분석한다.
+//         //https://spys.one/free-proxy-list/KR/
+//         //- 프록시 목록 중 익명 항목이 NOA(노아)라면 익명성을 보장하지 않는다. 그러므로 사용하지 않는다.
+
+//         let browser = await puppeteer.launch({headless : false, args:['--window-size=1920,1080', '--disable-notifications']});
+//         let page = await browser.newPage();
+//         await page.setViewport({
+//             width : 1080,
+//             height : 1080
+//         });
+//         await page.goto('https://spys.one/free-proxy-list/KR/');
+//         const proxies = await page.evaluate(()=>{
+//             const ips = Array.from(document.querySelectorAll('tr > td:first-of-type > .spy14')).map((v)=>v.textContent.replace(/document\.write\(.+\)/, ''));
+//             const types = Array.from(document.querySelectorAll('tr > td:nth-of-type(2)')).slice(5).map((v)=>v.textContent);
+//             const latencies = Array.from(document.querySelectorAll('tr > td:nth-of-type(6) .spy1')).map((v)=>v.textContent);
+
+//             return ips.map((v, i)=>{
+//                 return {
+//                     ip:v,
+//                     type : types[i],
+//                     latency : latencies[i]
+//                 }
+//             });
+//         });
+
+//         const filtered = proxies.filter((v)=>v.type.startsWith('HTTP')).sort((p, c) => p.latency - c.latency);
+//         console.log(filtered);
+
+//         await page.close();
+//         await browser.close();
+
+//         //2. 가장 빠른 프록시의 ip로 우회한다.
+//         //프록시를 통해 접속하려면 puppeteer.launch 함수의 args매개변수에 --proxy-server={ip} 를 추가해야한다.
+//         //실제로 해보니 HTTPS, HTTP (Squid)에서는 제대로 작동하지 않으므로 HTTP만 사용해야 한다.
+//         browser = await puppeteer.launch({
+//             headless : false, 
+//             args:['--window-size=1920,1080', '--disable-notifications', `--proxy-server=${filtered[3].ip}`],
+//         });
+//         page = await browser.newPage();
+//         await page.goto('https://www.naver.com/');
+
+//     }catch(e){
+//         console.error(e);
+//     }
+// };
+
+// crawler();
+
+//==================================================================================//
+//6-3 데이터베이스 연동하기
+//해당 강의에서는 사용할 수 있는 무료 프록시 목록을 데이터베이스에 저장하기위해 쓰인다.
+//1. sequlize, sequelize-cli 모듈 설치
+// - node.js에서 DB를 연동하기 위해 사용하는 모듈이다. (많이 사용된다.)
+// - sequelize-cli는 global로 설치되어야 한다. (npm i -g sequelize-cli)
+//2. MySQL에서 table을 생성한다. (create new schema... 메뉴 선택)
+//3. 프로젝트 폴더에서 sequeilze init 명령어를 실행한다.
+// - config, migrations, models, seeders 폴더가 프로젝트에 생성된다.
+//4. config/config.json의 모든 database과 password를 수정한다.
+/*
+    "password": 12345,
+    "database": "node-crawler",
+*/
+//5. models/index.js 파일 내용을 간단하게 변경한다.
+// - 해당 파일이 DB를 연결하는 부분이다.
+//6. models/proxy.js 파일을 생성하여 table을 만드는 코드를 작성한다.
+
+//!! 해당 예제를 실행할 때 config에 비밀번호를 입력해야 한다.
+
 const puppeteer = require('puppeteer');
 const dotenv = require('dotenv');
+
+const db = require('./models');
 dotenv.config(); //.env파일에 작성한 환경변수를 불러와 process.env 에 구성한다.
 
 const crawler = async ()=>{
-    try{
-        const browser = await puppeteer.launch({headless : false, args:['--window-size=1920,1080', '--disable-notifications']});
-        const page = await browser.newPage();
-        await page.setViewport({
-            width : 1080,
-            height : 1080
-        });
-        await page.goto('https://facebook.com');
 
-        await page.focus('#email');
-        //대문자 입력 방법
-        await page.click('#email');
-        await page.keyboard.down('ShiftLeft');
-        await page.keyboard.press('KeyZ');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyE');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyR');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyO');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyC');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyH');
-        await page.waitFor(1000);
-        await page.keyboard.press('KeyO');
-        await page.waitFor(1000);
-        await page.keyboard.up('ShiftLeft');
-        
+    //7. DB 연결
+    await db.sequelize.sync();
+
+    try{
+
     }catch(e){
         console.error(e);
     }
 };
 
 crawler();
+//==================================================================================//
