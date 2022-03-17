4be99eda60fc84e414f65f34d0180b4f9bcb3bef
unknown <coa2769@gmail.com>
2022-03-17 23:24:22 +0900

    React 공부(03)-ref 관련 예제 작성

--- React_book/basic-app/src/App.js
+++ React_book/basic-app/src/App.js
@@ -1,13 +1,19 @@
+import React, { Component } from 'react';
 import './App.css';
 // import Say from './component/Say';
-import EventPractice from './components/EventPractice';
+// import EventPractice from './components/EventPractice';
+// import ValidationSample from './components/ValidationSample';
+import ScrollBox from './components/ScrollBox';
 
-const App = () => {
-  return (
-    <div>
-      <EventPractice></EventPractice>
-    </div>
-  )
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <ScrollBox ref={(ref)=>this.scrollBox=ref}></ScrollBox>
+        <button onClick={()=>this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    )
+  }
 }
 
 export default App;
