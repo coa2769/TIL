20b6278f954d44ccbaf49204273ad53f0d44bd56
unknown <coa2769@gmail.com>
2022-03-18 23:55:54 +0900

    React 공부(04)-컴포넌트 반복 예제 작성

---
+++ React_book/basic-app/src/components/IterationSample.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const IterationSample = ()=>{
+    const [ names, setNames ] = useState([
+        {id : 1, text : '눈사람'},
+        {id : 2, text : '얼음'},
+        {id : 3, text : '눈'},
+        {id : 4, text : '바람'},
+    ])
+    // const [inputText ,]
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    return <ul>{nameList}</ul>
+}
+
+export default IterationSample;
