0ca65936fd8671139897fb1db5a30917a908a9c0
unknown <coa2769@gmail.com>
2022-03-20 21:13:06 +0900

    React 공부(05) - 라이프사이클 예제 작성

--- React_book/basic-app/src/App.js
+++ React_book/basic-app/src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 // import Say from './component/Say';
 // import EventPractice from './components/EventPractice';
 // import ValidationSample from './components/ValidationSample';
-import ScrollBox from './components/ScrollBox';
+// import ScrollBox from './components/ScrollBox';
+import LifeCycleSample from './components/LifeCycleSample';
+import ErrorBoundary from './components/ErrorBoundary';
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component{
+  state = {
+    color : '#000000'
+  }
+
+  handleClick = ()=>{
+    this.setState({
+      color : getRandomColor()
+    });
+  }
+
   render(){
     return (
       <div>
-        <ScrollBox ref={(ref)=>this.scrollBox=ref}></ScrollBox>
-        <button onClick={()=>this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}></LifeCycleSample>
+        </ErrorBoundary>
+        {/* <ScrollBox ref={(ref)=>this.scrollBox=ref}></ScrollBox> */}
+        {/* <button onClick={()=>this.scrollBox.scrollToBottom()}>맨 밑으로</button> */}
       </div>
     )
   }
--- React_book/basic-app/src/components/LifeCycleSample.js
+++ React_book/basic-app/src/components/LifeCycleSample.js
@@ -40,5 +40,42 @@ class LifeCycleSample extends Component{
         this.setState({ number : this.state.number + 1});
     }
 
-    getSnapshotBeforeUpdate()
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('getSnapshotBeforeUpdate');
+        if(prevProps.color !== this.props.color){
+            return this.myRef.style.color;
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('componentDidUpdate', prevProps, prevState);
+        if(snapshot){
+            console.log('업데이트되기 직전 색상', snapshot);
+        }
+    }
+
+    render(){
+        console.log('render');
+
+        const style = {
+            color : this.props.color
+        };
+
+        return (
+            <div>
+                {/* 존재하지 않는 props에 접근 */}
+                {this.props.missing.value}
+                <h1 style={style} ref={ref => this.myRef = ref}>
+                    {this.state.number}
+                </h1>
+                <p>color : {this.state.color}</p>
+                <button onClick={this.handleClick}>
+                    더하기
+                </button>
+            </div>
+        )
+    }
 }
+
+export default LifeCycleSample;
