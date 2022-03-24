7d707fc74c257825e0086a61c22a73e418032b54
unknown <coa2769@gmail.com>
2022-03-24 22:52:21 +0900

    typescript 강의 00~01 강의 정리

---
+++ typescript/why-ts/sample.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b){
+    return a + b;
+}
+
+let result = sum(10, 20);
+result.toLocaleString();
+
+//타입스크립트 같은 코드를 작성하기 위해서는 JSDoc을 사용해야 하나.
