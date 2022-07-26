7b186747ab769d7231be49af38accf6c54580b95
unknown <coa2769@gmail.com>
2022-07-26 22:57:49 +0900

    node 교과서 제2판 9장 예제 오류 수정

--- node 강의/ch 09 익스프레스로 SNS서비스 만들기/back-end/routes/user.js
+++ node 강의/ch 09 익스프레스로 SNS서비스 만들기/back-end/routes/user.js
@@ -5,6 +5,7 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+
 router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
     //팔로우할 사용자 데이터베이스 조회
@@ -24,5 +25,23 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
     next(error);
   }
 });
+
+// router.delete('/:id/follow', isLoggedIn, async (req, res, next)=>{
+//   try{
+//     //팔로우 해제할 사용자 데이버메이스 조회
+//     const user = await User.findOne({ where : { id : req.user.id  } });
+
+//     if(user){
+//       await user.removeFollowing(parseInt(req.params.id, 10));
+//       res.send('success');
+//     }else{
+//       res.status(404).send('no user');
+//     }
+//   } catch(error){
+//     console.error(error);
+//     next(error);
+//   }
+
+// });
 
 module.exports = router;
